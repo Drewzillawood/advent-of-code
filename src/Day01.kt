0fc7b8a24248e9aaaa329bdbce4c986f5dc4a274
Drew Underwood <drew@uwood.net>
2021-12-01 16:01:31 -0600

    Completed day 1!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
+    fun ints(input: List<String>) = input.map { it.toInt() }.toTypedArray()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        val intArr = ints(input)
+        for(i in intArr.indices) {
+            if (i < intArr.size - 1 && intArr[i] < intArr[i + 1]) {
+                count++
+            }
+        }
+        return count
+    }
+
+    fun window (input: List<Int>, i: Int): Int {
+        return input[i] + input[i + 1] + input[i + 2]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        val intArr = ints(input).toList()
+        for(i in intArr.indices) {
+            if(i > 0 && i < intArr.size - 3)
+                if (window(intArr, i) < window(intArr, i + 1))
+                    count++
+        }
+        return count
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))

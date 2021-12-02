ad0925ee3f71fc91a93e349b1c8a20c00589998b
Drew Underwood <drew@uwood.net>
2021-12-02 14:53:08 -0600

    Day 2 Part 2

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -11,12 +11,21 @@ fun main() {
     fun part1(input: List<String>): Int {
         var x = 0
         var y = 0
+        var aim = 0
         for(i in input) {
             val inputs = i.split(" ")
+            val displacement = inputs[1].toInt()
             when(parseDirection(inputs[0])) {
-                Direction.UP -> y -= inputs[1].toInt()
-                Direction.DOWN -> y += inputs[1].toInt()
-                Direction.FORWARD -> x += inputs[1].toInt()
+                Direction.UP -> {
+                    aim -= displacement
+                }
+                Direction.DOWN -> {
+                    aim += displacement
+                }
+                Direction.FORWARD -> {
+                    x += displacement
+                    y = if(aim == 0) y else y + displacement * aim
+                }
             }
         }
         return x * y
